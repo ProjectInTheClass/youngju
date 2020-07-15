9ef926f14240ac51f97821f54589343bd617c86e
seriiin <csl602@likelion.org>
2020-07-15 12:07:38 +0900

    Initial Commit

---
+++ WebtoonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebtoonApp
+//
+//  Created by GW_14 on 15/07/2020.
+//  Copyright © 2020 GW_14. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
